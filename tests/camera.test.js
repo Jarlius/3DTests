8b87a09cee993c7388a37d23944819c4ad518fec
Jarl Silvén <jarl.silven@gmail.com>
2020-11-22 22:11:15 +0100

    :hammer: Camera tests create angles from coordinates

--- tests/camera.test.js
+++ tests/camera.test.js
@@ -28,8 +28,32 @@ const cartesianToPolar = (x,y,z) => {
 	return {theta: theta + quadrant, phi: phi + updown};
 };
 
-// x argument and x_diff argument is 2 and -2, can be improved
-const test = cartesianToPolar(2,-2,-4);
-console.log(toDeg(test.theta),toDeg(test.phi));
-const res = handler.checkAngles(test,{theta:0,phi:0},-2);
+// get screen angles between camera center and click
+// TODO test more angles for +/- errors
+const getAngles = (cam,click) =>  {
+	const phi_c = click.phi-Math.PI/2;
+	const theta_d = click.theta-cam.theta;
+
+	const phi = Math.atan(
+		Math.sin(phi_c) / // far leg
+		( Math.cos(theta_d) * Math.cos(phi_c) ) // near leg
+	);
+	const theta = Math.atan( Math.cos(phi) * Math.tan(theta_d) );
+	
+	return {
+		theta: theta,
+		phi: phi-(cam.phi-Math.PI/2) 
+	};
+};
+
+// x argument and x_diff argument is 2 and -2, can be automated later
+const cam = cartesianToPolar(2,-2,-4);
+const click = cartesianToPolar(2,-2,-2);
+const test = getAngles(cam,click);
+
+console.log('resulting click angle should be close to -14.9,12.8');
+console.log('theta',toDeg(test.theta));
+console.log('phi',toDeg(test.phi));
+
+const res = handler.checkAngles(cam,{theta:test.theta,phi:test.phi},-2); // here is the -2 x_diff
 console.log(res);
