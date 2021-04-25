12f9607ced38a666bbefed45ce20008d20b03eae
Jarl Silvén <jarl.silven@gmail.com>
2021-04-25 19:25:15 +0200

    :wrench: use class syntax for classes instead of function

--- public/js/states.src.js
+++ public/js/states.src.js
@@ -1,38 +1,38 @@
 const ObjectMaker = require('./objects.src.js');
 
-function Normal() {
-	this.clickLeftDown = () => {
+class Normal {
+	clickLeftDown() {
 		console.log("normal mode");
-	};
-	this.pressV = (grid,scene) => {
+	}
+	pressV(grid,scene) {
 		const newgrid = ObjectMaker.makeGrid();
 		scene.add( newgrid );
 		return {
 			state: new BuildBase(),
 			grid: newgrid
 		};
-	};
-};
+	}
+}
 
-function BuildBase() {
-	this.clickLeftDown = () => {
+class BuildBase {
+	clickLeftDown() {
 		console.log("floor build mode");
-	};
-	this.pressV = (grid,scene) => {
+	}
+	pressV(grid,scene) {
 		scene.remove(grid);
 		return {
 			state: new Normal(),
 			grid: null
 		};
-	};
-	this.pressPlus = (grid) => {
+	}
+	pressPlus(grid) {
 		ObjectMaker.incLevel(1);
 		grid.position.set( 0, ObjectMaker.getLevel(), 0 );
-	};
-	this.pressMinus = (grid) => {
+	}
+	pressMinus(grid) {
 		ObjectMaker.incLevel(-1);
 		grid.position.set( 0, ObjectMaker.getLevel(), 0 );
-	};
-};
+	}
+}
 
 exports.getStartingState = Normal;
